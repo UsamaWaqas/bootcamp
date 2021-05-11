e340dee5f201950c4844639b4a0a3000489c0e52
UsamaWaqas <usamawaqas.m@gmail.com>
2021-05-12 02:09:27 +0500

    updated

---
+++ bootcamp-react-app/demoapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> asalmulikum! i am form pakistan</h1>
+    </div>
+  );
+}
+
+export default App;
--- bootcamp-react-app/src/App.js
+++ bootcamp-react-app/src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+
 
 function App() {
-  return (
+  return(
     <div >
     <h1>hy i am usama and i am trying to learn ract libery of javascript </h1>
     </div>
